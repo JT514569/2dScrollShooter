6b0540e818d9bdf25646bffc7fae6364ae69f8d1
ToogoodGuy <514569@chrome.escg.ac.uk>
2023-10-23 17:44:48 +0100

    Monday changes. About to start programming proper rate of fire.

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -5,6 +5,18 @@ using UnityEngine;
 public class EnemyMove : MonoBehaviour
 {
     [SerializeField] private float speed;
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D body;
+    public static Animator anim;
+
+    private void Awake()
+    {
+        //Grab refs for rigidbody and animator
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
 
     // Update is called once per frame
     private void Update()
@@ -16,10 +28,12 @@ public class EnemyMove : MonoBehaviour
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (gameObject.tag == "Enemy")
+        {
             if (collision.tag == "Delete")
             {
                 Debug.Log("Deleted");
                 Destroy(this.gameObject);
             }
+        }
     }
 }
